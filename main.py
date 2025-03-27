dca1a229b05f36365cc830b74347d2c943db1cae
Swayam Kuckreja <swayam.kuckreja@gmail.com>
2025-03-27 09:26:20 +0100

    added main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+# Packages and imports
+import numpy as np
+
+# Main function
+def main():
+    print('hello world')
+    return ...
+
+# Main function call
+if __name__ == '__main__':
+    main()
+   
