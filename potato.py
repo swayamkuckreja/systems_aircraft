cf76d934d28f5e59fdc88c2d4c4e3250bf36bd29
f.o.olsson@student.tudelft.nl <f.o.olsson@student.tudelft.nl>
2025-03-30 16:46:57 +0200

    enhance CG calculations and visualization by refining fuel loading logic and plotting mechanics in potato.py

--- potato.py
+++ potato.py
@@ -23,10 +23,14 @@ print(f"Total cargo weight: {m_cargo} kg")
 x_cg_cargo1 = 4.33013  # CG location for cargo 1 (meters)
 x_cg_cargo2 = 20.89227 # CG location for cargo 2 (meters)
 
-# Constant(fuel)
+# Constant (fuel)
 m_fuel = ac.mtow - ac.oew - m_cargo - m_totpassengers  # Total fuel weight (kg)
-x_fuel = 14.5  # CG location for fuel (meters)
+x_cg_fuel = 14.5  # CG location for fuel (meters)
 print(f"Total fuel weight: {m_fuel} kg")
+
+# Constants (MAC)
+x_mac = 12.0
+l_mac = 4
 
 # Function to calculate seat CG
 def cg_seat(i):
@@ -105,12 +109,13 @@ print(f'Cargo loaded cg = {x_cg_cargoloaded} m, and cargoloaded mass is {m_cargo
 line3x.append(x_cg)
 line3y.append(m)
 # Fill window seats first (front to back)
-for i in range(rows+1):
+for i in range(rows):
     for j in columns_1:
         x_cg = cg_formula(x_cg, cg_seat(i), m_passenger, m)
         m += m_passenger
         line3x.append(x_cg)
         line3y.append(m)
+        print(f'seat {i,j}, weight is {m}')
 
 plt.plot(line3x,line3y)
 
@@ -120,7 +125,7 @@ m = m_cargoloaded
 # Fill window seats next (back to front)
 line4x.append(x_cg)
 line4y.append(m)
-for i in range(rows, -1, -1):
+for i in range(rows-1, -1, -1):
     for j in columns_2:
         x_cg = cg_formula(x_cg, cg_seat(i), m_passenger, m)
         m += m_passenger
@@ -139,35 +144,70 @@ print(f'cg after window loaded = {x_cg_windowloaded}, m after window loaded = {m
 line5x.append(x_cg)
 line5y.append(m)
 # Fill window seats first (front to back)
-for i in range(rows+1):
+for i in range(rows):
     for j in columns_1:
         x_cg = cg_formula(x_cg, cg_seat(i), m_passenger, m)
         m += m_passenger
-        line3x.append(x_cg)
-        line3y.append(m)
+        line5x.append(x_cg)
+        line5y.append(m)
 
-plt.plot(line3x,line3y)
+plt.plot(line5x,line5y)
 
-x_cg = x_cg_cargoloaded 
-m = m_cargoloaded
+x_cg = x_cg_windowloaded
+m = m_windowloaded
 
 # Fill window seats next (back to front)
-line4x.append(x_cg)
-line4y.append(m)
-for i in range(rows, -1, -1):
+line6x.append(x_cg)
+line6y.append(m)
+for i in range(rows-1, -1, -1):
     for j in columns_2:
         x_cg = cg_formula(x_cg, cg_seat(i), m_passenger, m)
         m += m_passenger
-        line4x.append(x_cg)
-        line4y.append(m)
+        line6x.append(x_cg)
+        line6y.append(m)
 
-plt.plot(line4x,line4y)
+plt.plot(line6x,line6y)
+
+print(f'cg after aisle loaded = {x_cg}, m after window loaded = {m}')
+
+'''Fuel loading'''
+
+line7x.append(x_cg)
+line7y.append(m)
+for i in range(int(m_fuel)):
+    x_cg = cg_formula(x_cg, x_cg_fuel, 1, m)
+    m += 1 # Increment mass 1kg
+    line7x.append(x_cg)
+    line7y.append(m)
+
+plt.plot(line7x,line7y)
+
+# Plot formatting
+plt.ylabel('mass [kg]')
+plt.xlabel('xcg [m]')
+plt.title('CG Location to Tip of Nose Cone vs Mass')
+plt.grid()
+
 plt.show()
-
 # Display the final CG
-print(f"Final CG Location: {x_cg:.2f} meters")
+print(f"Final CG Location: {x_cg:.2f} meters and final mass: {m:.2f} kg")
 
+'''Do it with respect to MAC'''
 
+line1x = np.array(line1x)
+line1y = np.array(line1y)
+line2x = np.array(line2x)
+line2y = np.array(line2y)
+line3x = np.array(line3x)
+line3y = np.array(line3y)
+line4x = np.array(line4x)
+line4y = np.array(line4y)
+line5x = np.array(line5x)
+line5y = np.array(line5y)
+line6x = np.array(line6x)
+line6y = np.array(line6y)
+line7x = np.array(line7x)
+line7y = np.array(line7y)
 
 
 
