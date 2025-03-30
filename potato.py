3d5cce9b15e6b297b52c4dc5ee5257f5e4749e9e
f.o.olsson@student.tudelft.nl <f.o.olsson@student.tudelft.nl>
2025-03-30 16:52:24 +0200

    add cg_to_mac function and enhance plotting for CG vs Mass visualization

--- potato.py
+++ potato.py
@@ -40,6 +40,9 @@ def cg_seat(i):
 def cg_formula(old_cg, new_cg, new_weight, old_weight):
     return (old_cg * old_weight + new_cg * new_weight) / (old_weight + new_weight)
 
+def cg_to_mac(cg):
+    return ((cg - x_mac) / l_mac) * 100
+
 # Initialize CG and weight
 x_cg = x_cg_oew
 m = ac.oew
@@ -61,51 +64,43 @@ line1x.append(x_cg)
 line1y.append(m)
 for i in range(int(m_cargo1)):
     x_cg = cg_formula(x_cg, x_cg_cargo1, 1, m)
-    m += 1 # Increment mass 1kg
+    m += 1  # Increment mass 1kg
     line1x.append(x_cg)
     line1y.append(m)
 
-line1x.append(x_cg)
-line1y.append(m)
 for i in range(int(m_cargo2)):
     x_cg = cg_formula(x_cg, x_cg_cargo2, 1, m)
-    m += 1 # Increment mass 1kg
+    m += 1  # Increment mass 1kg
     line1x.append(x_cg)
     line1y.append(m)
 
-plt.plot(line1x,line1y)
-
-# Zero to oew again
-
+# Reset to OEW
 x_cg = x_cg_oew
 m = ac.oew
+
 # Back to front loaded
 line2x.append(x_cg)
 line2y.append(m)
 for i in range(int(m_cargo2)):
     x_cg = cg_formula(x_cg, x_cg_cargo2, 1, m)
-    m += 1 # Increment mass 1kg
+    m += 1  # Increment mass 1kg
     line2x.append(x_cg)
     line2y.append(m)
 
-line2x.append(x_cg)
-line2y.append(m)
 for i in range(int(m_cargo1)):
     x_cg = cg_formula(x_cg, x_cg_cargo1, 1, m)
-    m += 1 # Increment mass 1kg
+    m += 1  # Increment mass 1kg
     line2x.append(x_cg)
     line2y.append(m)
-
-plt.plot(line2x,line2y)
 
 x_cg_cargoloaded = x_cg
 m_cargoloaded = m
 
-print(f'Cargo loaded cg = {x_cg_cargoloaded} m, and cargoloaded mass is {m_cargoloaded} kg')
+print(f'Cargo loaded CG = {x_cg_cargoloaded:.2f} m, and cargo loaded mass is {m_cargoloaded:.2f} kg')
 
 '''Passenger loading'''
 
-'''Window seats'''
+# Window seats
 line3x.append(x_cg)
 line3y.append(m)
 # Fill window seats first (front to back)
@@ -115,78 +110,79 @@ for i in range(rows):
         m += m_passenger
         line3x.append(x_cg)
         line3y.append(m)
-        print(f'seat {i,j}, weight is {m}')
 
-plt.plot(line3x,line3y)
-
-x_cg = x_cg_cargoloaded 
+# Reset to cargo loaded state
+x_cg = x_cg_cargoloaded
 m = m_cargoloaded
 
 # Fill window seats next (back to front)
 line4x.append(x_cg)
 line4y.append(m)
-for i in range(rows-1, -1, -1):
-    for j in columns_2:
+for i in range(rows - 1, -1, -1):
+    for j in columns_1:
         x_cg = cg_formula(x_cg, cg_seat(i), m_passenger, m)
         m += m_passenger
         line4x.append(x_cg)
         line4y.append(m)
 
-plt.plot(line4x,line4y)
-
 x_cg_windowloaded = x_cg
 m_windowloaded = m
 
-print(f'cg after window loaded = {x_cg_windowloaded}, m after window loaded = {m_windowloaded}')
+print(f'CG after window seats loaded = {x_cg_windowloaded:.2f} m, mass after window seats loaded = {m_windowloaded:.2f} kg')
 
-'''Aisle seats'''
-
+# Aisle seats
 line5x.append(x_cg)
 line5y.append(m)
-# Fill window seats first (front to back)
+# Fill aisle seats first (front to back)
 for i in range(rows):
-    for j in columns_1:
+    for j in columns_2:
         x_cg = cg_formula(x_cg, cg_seat(i), m_passenger, m)
         m += m_passenger
         line5x.append(x_cg)
         line5y.append(m)
 
-plt.plot(line5x,line5y)
-
+# Reset to window loaded state
 x_cg = x_cg_windowloaded
 m = m_windowloaded
 
-# Fill window seats next (back to front)
+# Fill aisle seats next (back to front)
 line6x.append(x_cg)
 line6y.append(m)
-for i in range(rows-1, -1, -1):
+for i in range(rows - 1, -1, -1):
     for j in columns_2:
         x_cg = cg_formula(x_cg, cg_seat(i), m_passenger, m)
         m += m_passenger
         line6x.append(x_cg)
         line6y.append(m)
 
-plt.plot(line6x,line6y)
-
-print(f'cg after aisle loaded = {x_cg}, m after window loaded = {m}')
+print(f'CG after aisle seats loaded = {x_cg:.2f} m, mass after aisle seats loaded = {m:.2f} kg')
 
 '''Fuel loading'''
-
 line7x.append(x_cg)
 line7y.append(m)
 for i in range(int(m_fuel)):
     x_cg = cg_formula(x_cg, x_cg_fuel, 1, m)
-    m += 1 # Increment mass 1kg
+    m += 1  # Increment mass 1kg
     line7x.append(x_cg)
     line7y.append(m)
 
-plt.plot(line7x,line7y)
+# Plot all lines with proper labels
+plt.figure(figsize=(10, 6))
+plt.plot(line1x, line1y, label="Cargo Front to Back")
+plt.plot(line2x, line2y, label="Cargo Back to Front")
+plt.plot(line3x, line3y, label="Window Seats Front to Back")
+plt.plot(line4x, line4y, label="Window Seats Back to Front")
+plt.plot(line5x, line5y, label="Aisle Seats Front to Back")
+plt.plot(line6x, line6y, label="Aisle Seats Back to Front")
+plt.plot(line7x, line7y, label="Fuel Loading")
 
 # Plot formatting
-plt.ylabel('mass [kg]')
-plt.xlabel('xcg [m]')
-plt.title('CG Location to Tip of Nose Cone vs Mass')
+plt.ylabel('Mass [kg]')
+plt.xlabel('CG Location [meters]')
+plt.title('CG Location vs Mass')
+plt.legend()
 plt.grid()
+plt.tight_layout()
 
 plt.show()
 # Display the final CG
@@ -194,20 +190,42 @@ print(f"Final CG Location: {x_cg:.2f} meters and final mass: {m:.2f} kg")
 
 '''Do it with respect to MAC'''
 
-line1x = np.array(line1x)
+line1x = np.array([cg_to_mac(x) for x in line1x])
 line1y = np.array(line1y)
-line2x = np.array(line2x)
+line2x = np.array([cg_to_mac(x) for x in line2x])
 line2y = np.array(line2y)
-line3x = np.array(line3x)
+line3x = np.array([cg_to_mac(x) for x in line3x])
 line3y = np.array(line3y)
-line4x = np.array(line4x)
+line4x = np.array([cg_to_mac(x) for x in line4x])
 line4y = np.array(line4y)
-line5x = np.array(line5x)
+line5x = np.array([cg_to_mac(x) for x in line5x])
 line5y = np.array(line5y)
-line6x = np.array(line6x)
+line6x = np.array([cg_to_mac(x) for x in line6x])
 line6y = np.array(line6y)
-line7x = np.array(line7x)
+line7x = np.array([cg_to_mac(x) for x in line7x])
 line7y = np.array(line7y)
+
+# Plot all lines with respect to MAC
+plt.plot(line1x, line1y, label="Cargo Front to Back")
+plt.plot(line2x, line2y, label="Cargo Back to Front")
+plt.plot(line3x, line3y, label="Window Seats Front to Back")
+plt.plot(line4x, line4y, label="Window Seats Back to Front")
+plt.plot(line5x, line5y, label="Aisle Seats Front to Back")
+plt.plot(line6x, line6y, label="Aisle Seats Back to Front")
+plt.plot(line7x, line7y, label="Fuel Loading")
+
+# Plot formatting
+plt.ylabel('mass [kg]')
+plt.xlabel('xcg [% MAC]')
+plt.title('CG Location to MAC vs Mass')
+plt.legend()
+plt.grid()
+
+plt.show()
+
+
+
+
 
 
 
